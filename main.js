ddac0cebced6e981ce3b2f24784e56e6f0c3d4b5
tisukRoman <tisukroman@gmail.com>
2023-02-20 16:40:01 +0200

    Sensor added

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 animate();
 
 function animate() {
-  car.update();
+  car.update(road.borders);
+
   canvas.height = window.innerHeight;
 
   ctx.save();
-  ctx.translate(0, -car.y + canvas.height * 0.5);
+  ctx.translate(0, -car.y + canvas.height * 0.7);
 
   road.draw(ctx);
   car.draw(ctx);
