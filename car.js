9f3218a3b4daa0c5ff947126b9269741ef3fa448
tisukRoman <tisukroman@gmail.com>
2023-02-20 13:47:36 +0200

    Car driving mechanics

---
+++ car.js
@@ -0,0 +1,72 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.maxSpeed = 3;
+    this.acceleration = 0.1;
+    this.friction = 0.05;
+    this.angle = 0;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+    // updating speed
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      // updating angle
+      if (this.controls.left && Math.abs(this.speed) > this.friction) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right && Math.abs(this.speed) > this.friction) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    // max speed constraints
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    // friction
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+
+    ctx.restore();
+  }
+}
